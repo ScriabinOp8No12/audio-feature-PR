e64d6bd086e26ba60e0a92d14151edacd40a31de
nharwit <nharwit@gmail.com>
2025-05-24 14:59:13 -0600

    Changed capturing race -> liberty race for lesson 8 section, added small amount of padding to bottom button container to avoid it going off the screen, especially for lesson 8

--- src/views/Lessons/PuzzleSections.ts
+++ src/views/Lessons/PuzzleSections.ts
@@ -20,7 +20,7 @@ import { Capturing } from "./Lesson8Puzzles/Capturing";
 import { LifeAndDeath } from "./Lesson8Puzzles/LifeAndDeath";
 import { Ladder } from "./Lesson8Puzzles/Ladder";
 import { Net } from "./Lesson8Puzzles/Net";
-import { Semeai } from "./Lesson8Puzzles/Semeai";
+import { LibertyRace } from "./Lesson8Puzzles/LibertyRace";
 import { Endgame } from "./Lesson8Puzzles/Endgame";
 import { Tesuji } from "./Lesson8Puzzles/Tesuji";
 import { Random } from "./Lesson8Puzzles/Random";
@@ -31,7 +31,7 @@ export const sectionKeys = [
     "lifeanddeath",
     "ladder",
     "net",
-    "semeai",
+    "libertyrace",
     "tesuji",
     "random",
 ] as const;
@@ -42,7 +42,7 @@ export const puzzleSectionMap: Record<(typeof sectionKeys)[number], Array<typeof
     lifeanddeath: LifeAndDeath,
     ladder: Ladder,
     net: Net,
-    semeai: Semeai,
+    libertyrace: LibertyRace,
     tesuji: Tesuji,
     random: Random,
 };
@@ -53,7 +53,7 @@ export const sectionDisplayNames: Record<(typeof sectionKeys)[number], string> =
     lifeanddeath: "Life and Death",
     ladder: "Ladder",
     net: "Net",
-    semeai: "Semeai",
+    libertyrace: "Liberty Race",
     tesuji: "Tesuji",
     random: "Random",
 };
