cafb2da16826e02c140bd0fbefea6d9703e32ddc
nharwit <nharwit@gmail.com>
2025-07-27 00:25:48 -0600

    Finished adding audio to module 4 and module 5

--- src/views/Lessons/Module4.tsx
+++ src/views/Lessons/Module4.tsx
@@ -20,14 +20,49 @@ import { Content } from "./Content";
 import { PuzzleConfig, Goban, JGOFNumericPlayerColor } from "goban";
 
 class Module4 extends Content {
-    constructor() {
+    audioRef: React.RefObject<HTMLAudioElement>;
+    audioUrl: string;
+
+    constructor(audioUrl: string) {
         super();
+        this.audioRef = React.createRef();
+        this.audioUrl = audioUrl;
+    }
+
+    playAudio = async () => {
+        const audio = this.audioRef.current;
+        if (audio) {
+            await audio.play();
+        }
+    };
+
+    componentWillUnmount() {
+        const audio = this.audioRef.current;
+        if (audio) {
+            audio.pause();
+            audio.currentTime = 0;
+        }
     }
 }
 
 class Page1 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708636014/audio-slices-less-pauses/slice64_less_pauses_revised_lppayu.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 Here's a situation that sometimes comes up. Notice that the White stone at A is in
                 atari?
@@ -49,8 +84,24 @@ class Page1 extends Module4 {
 }
 
 class Page2 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708636318/audio-slice-less-pauses-COMBINED/slice65_and_66_combined_j2mjda.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>Since Blue is capturing a stone and getting a liberty, playing at B is allowed.</p>,
             <p>But now Blue is in atari...</p>,
         ];
@@ -70,8 +121,23 @@ class Page2 extends Module4 {
 }
 
 class Page3 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708636476/audio-slices-less-pauses/slice67_less_pauses_revised_hg7s10.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>So White could play at A again and capture Blue. But now White is in atari...</p>,
         ];
     }
@@ -90,8 +156,24 @@ class Page3 extends Module4 {
 }
 
 class Page4 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708636656/audio-slice-less-pauses-COMBINED/slice68_69_and_70combined_ffqsh6.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>So Blue could capture, but then would be in atari again...</p>,
             <p>This could go on forever, so there is a special rule to cover it.</p>,
             <p>
@@ -115,8 +197,23 @@ class Page4 extends Module4 {
 }
 
 class Page5 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708636813/audio-slices-less-pauses/slice71_less_pauses_revised_e95kry.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 So the first player to capture in a position like this starts what is called a "ko".
                 On this board, Blue would start a ko by capturing at 1.
@@ -138,8 +235,23 @@ class Page5 extends Module4 {
 }
 
 class Page6 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708472600/audio-slices-less-pauses/slice70_less_pauses_obbwtz.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 The ko rule prevents a player from immediately recapturing. That would repeat the
                 board. But it only affects the first move after a capture.
@@ -162,8 +274,24 @@ class Page6 extends Module4 {
 }
 
 class Page7 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708637273/audio-slice-less-pauses-COMBINED/slice73_and_74_combined_mscsc1.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 So White can play anywhere on the board except at the triangled point. Perhaps White
                 will play at 2, and then Blue might play at say, 3.
@@ -199,8 +327,24 @@ class Page7 extends Module4 {
 }
 
 class Page8 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708472604/audio-slices-less-pauses/slice73_less_pauses_k2puq1.wav",
+        );
+    }
+
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 Since the board has changed, a play at A is allowed. Now it’s Blue's turn to be
                 caught by the rule of ko. Blue can play anywhere except the triangled point.
@@ -225,8 +369,23 @@ class Page8 extends Module4 {
 }
 
 class Page9 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708472598/audio-slices-less-pauses/slice74_less_pauses_tsdqt4.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>So perhaps Blue will play at 1 to connect two stones and prevent a cut by White.</p>,
         ];
     }
@@ -246,8 +405,23 @@ class Page9 extends Module4 {
 }
 
 class Page10 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708472612/audio-slices-less-pauses/slice75_less_pauses_d1dylj.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 If White thinks the ko is more important, it can be filled in with 2. In this case,
                 Blue actually gets a good solid position by playing at 3. So each side got
@@ -272,8 +446,23 @@ class Page10 extends Module4 {
 }
 
 class Page11 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708637886/audio-slices-less-pauses/slice78_less_pauses_revised_tyedip.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 Sometimes winning the ko can affect the life of a whole group. If Blue can capture
                 and then fill the ko at A, the group will make two eyes. If White wins, all the Blue
@@ -296,8 +485,23 @@ class Page11 extends Module4 {
 }
 
 class Page12 extends Module4 {
+    constructor() {
+        super(
+            "https://res.cloudinary.com/dn8rdavoi/video/upload/v1708637983/audio-slices-less-pauses/slice79_less_pauses_revised_m0msdr.wav",
+        );
+    }
     text(): JSX.Element | Array<JSX.Element> {
         return [
+            <button key="playButton" onClick={this.playAudio}>
+                Play Audio
+            </button>,
+            <audio
+                key="audioElement"
+                ref={this.audioRef}
+                style={{ visibility: "hidden" }}
+                autoPlay={true}
+                src={this.audioUrl}
+            ></audio>,
             <p>
                 Sometimes a ko is meaningless and only affects the stone in atari. Don't worry about
                 ko too much for right now, just start playing some games and you'll learn more as
